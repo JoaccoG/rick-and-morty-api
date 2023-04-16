626012cfd56f72a539e5cf3906472101940f4cf1
JoaccoG <joaquingodoy2407@gmail.com>
2023-04-16 06:11:08 +0200

    Fix format on src/store folder

--- src/store/reducers/appReducer.test.ts
+++ src/store/reducers/appReducer.test.ts
@@ -1,63 +1,63 @@
-import { AppState } from "../../types/appInterfaces";
-import { ActionTypes, AppActions } from "../actions/actions";
-import appReducer from "./appReducer";
+import { AppState } from '../../types/appInterfaces';
+import { ActionTypes, AppActions } from '../actions/actions';
+import appReducer from './appReducer';
 
-describe("Given the app reducer", () => {
+describe('Given the app reducer', () => {
   const mockDefault: AppState = {
     planets: [],
     characters: [],
     planetDetails: {
       id: 0,
-      name: "",
-      type: "",
-      dimension: "",
-      created: "",
-      residents: [""],
+      name: '',
+      type: '',
+      dimension: '',
+      created: '',
+      residents: [''],
     },
     characterDetails: {
       id: 0,
-      name: "",
-      status: "",
-      species: "",
-      gender: "",
-      created: "",
-      image: "",
-      url: "",
+      name: '',
+      status: '',
+      species: '',
+      gender: '',
+      created: '',
+      image: '',
+      url: '',
     },
     currentPagePlanets: 1,
     currentPageCharacters: 1,
     favCharacter: {
       id: 1,
-      name: "",
-      status: "",
-      species: "",
-      gender: "",
-      created: "",
-      image: "",
-      url: "",
+      name: '',
+      status: '',
+      species: '',
+      gender: '',
+      created: '',
+      image: '',
+      url: '',
       rating: 0,
     },
     favCharacters: [],
   };
-  const mockPlanets: AppState["planets"] = [];
-  const mockCharacters: AppState["characters"] = [];
-  const mockFavCharacters: AppState["favCharacters"] = [
+  const mockPlanets: AppState['planets'] = [];
+  const mockCharacters: AppState['characters'] = [];
+  const mockFavCharacters: AppState['favCharacters'] = [
     {
       id: 1,
-      name: "Rick Sanchez",
-      created: "",
-      gender: "",
-      image: "",
+      name: 'Rick Sanchez',
+      created: '',
+      gender: '',
+      image: '',
       rating: 0,
-      species: "",
-      status: "",
-      url: "",
+      species: '',
+      status: '',
+      url: '',
     },
   ];
 
   test("When the reducer goes to default case, then the new state shouldn't change", () => {
     const defaultAction = {
-      type: "",
+      type: '',
     };
     const updateState = appReducer(
       mockDefault,
@@ -66,7 +66,7 @@ describe("Given the app reducer", () => {
     expect(updateState).toEqual(mockDefault);
   });
 
-  test("When the reducer receives GET_CHARACTERS action, then the state of the global context should change", () => {
+  test('When the reducer receives GET_CHARACTERS action, then the state of the global context should change', () => {
     const charactersAction: AppActions = {
       type: ActionTypes.GET_CHARACTERS,
       payload: mockCharacters,
@@ -75,7 +75,7 @@ describe("Given the app reducer", () => {
     expect(updateState).toEqual(mockDefault);
   });
 
-  test("When the reducer receives GET_PLANETS action, then the state of the global context should change", () => {
+  test('When the reducer receives GET_PLANETS action, then the state of the global context should change', () => {
     const planetsAction: AppActions = {
       type: ActionTypes.GET_PLANETS,
       payload: mockPlanets,
@@ -84,7 +84,7 @@ describe("Given the app reducer", () => {
     expect(updateState).toEqual(mockDefault);
   });
 
-  test("When the reducer receives NEXT_PAGE_CHARACTERS action, then the pagination should be one number higher than before", () => {
+  test('When the reducer receives NEXT_PAGE_CHARACTERS action, then the pagination should be one number higher than before', () => {
     const charactersAction: AppActions = {
       type: ActionTypes.NEXT_PAGE_CHARACTERS,
       payload: 10,
@@ -95,7 +95,7 @@ describe("Given the app reducer", () => {
     );
   });
 
-  test("When the reducer receives PREV_PAGE_CHARACTERS action, then the pagination should be one number lower than before", () => {
+  test('When the reducer receives PREV_PAGE_CHARACTERS action, then the pagination should be one number lower than before', () => {
     const charactersAction: AppActions = {
       type: ActionTypes.PREV_PAGE_CHARACTERS,
     };
@@ -103,7 +103,7 @@ describe("Given the app reducer", () => {
     expect(updateState.currentPageCharacters).toBe(1);
   });
 
-  test("When the reducer receives NEXT_PAGE_PLANETS action, then the pagination should be one number higher than before", () => {
+  test('When the reducer receives NEXT_PAGE_PLANETS action, then the pagination should be one number higher than before', () => {
     const planetsAction: AppActions = {
       type: ActionTypes.NEXT_PAGE_PLANETS,
       payload: 10,
@@ -114,7 +114,7 @@ describe("Given the app reducer", () => {
     );
   });
 
-  test("When the reducer receives PREV_PAGE_PLANETS action, then the pagination should be one number lower than before", () => {
+  test('When the reducer receives PREV_PAGE_PLANETS action, then the pagination should be one number lower than before', () => {
     const planetsAction: AppActions = {
       type: ActionTypes.PREV_PAGE_PLANETS,
     };
@@ -161,7 +161,7 @@ describe("Given the app reducer", () => {
     expect(updateState.currentPagePlanets).toBe(mockDefault.currentPagePlanets);
   });
 
-  test("When the reducer receives GET_CHARACTER_DETAILS action, then the state should change with the selected character", () => {
+  test('When the reducer receives GET_CHARACTER_DETAILS action, then the state should change with the selected character', () => {
     const charactersAction: AppActions = {
       type: ActionTypes.GET_CHARACTER_DETAILS,
       payload: mockCharacters[0],
@@ -170,7 +170,7 @@ describe("Given the app reducer", () => {
     expect(updateState.characterDetails).toEqual(mockCharacters[0]);
   });
 
-  test("When the reducer receives GET_PLANET_DETAILS action, then the state should change with the selected planet", () => {
+  test('When the reducer receives GET_PLANET_DETAILS action, then the state should change with the selected planet', () => {
     const planetsAction: AppActions = {
       type: ActionTypes.GET_PLANET_DETAILS,
       payload: mockPlanets[0],
@@ -179,7 +179,7 @@ describe("Given the app reducer", () => {
     expect(updateState.planetDetails).toEqual(mockCharacters[0]);
   });
 
-  test("When the reducer receives UPDATE_FAV_CHARACTERS action, then the state should with the new favorite characters data", () => {
+  test('When the reducer receives UPDATE_FAV_CHARACTERS action, then the state should with the new favorite characters data', () => {
     const favCharactersAction: AppActions = {
       type: ActionTypes.UPDATE_FAV_CHARACTERS,
       payload: mockFavCharacters,
