f499b888c8c78cf644b610dfb7660b67c20c6e0c
JoaccoG <joaquingodoy2407@gmail.com>
2023-04-16 06:10:44 +0200

    Fix format on src/mocks folder

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,65 +1,65 @@
 // src/mocks/handlers.js
-import { rest } from "msw";
+import { rest } from 'msw';
 
 export const handlers = [
-  rest.get("https://rickandmortyapi.com/api/location", (_req, res, ctx) => {
+  rest.get('https://rickandmortyapi.com/api/location', (_req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({
         results: [
           {
             id: 1,
-            name: "Earth (C-137)",
+            name: 'Earth (C-137)',
           },
         ],
       })
     );
   }),
-  rest.get("https://rickandmortyapi.com/api/character", (_req, res, ctx) => {
+  rest.get('https://rickandmortyapi.com/api/character', (_req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({
         results: [
           {
             id: 1,
-            name: "Rick Sanchez",
+            name: 'Rick Sanchez',
           },
         ],
       })
     );
   }),
-  rest.get("https://rickandmortyapi.com/api/location/1", (_req, res, ctx) => {
+  rest.get('https://rickandmortyapi.com/api/location/1', (_req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({
         id: 1,
-        name: "Earth (C-137)",
+        name: 'Earth (C-137)',
       })
     );
   }),
-  rest.get("https://rickandmortyapi.com/api/character/1", (_req, res, ctx) => {
+  rest.get('https://rickandmortyapi.com/api/character/1', (_req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({
         id: 1,
-        name: "Rick Sanchez",
+        name: 'Rick Sanchez',
       })
     );
   }),
   rest.get(
-    "https://private-api-adzv.onrender.com/characters",
+    'https://private-api-adzv.onrender.com/characters',
     (_req, res, ctx) => {
       return res(
         ctx.json([
           {
             id: 1,
-            name: "Rick Sanchez",
-            status: "",
-            species: "",
-            gender: "",
-            image: "",
-            url: "",
-            created: "",
+            name: 'Rick Sanchez',
+            status: '',
+            species: '',
+            gender: '',
+            image: '',
+            url: '',
+            created: '',
             rating: 0,
           },
         ])
@@ -67,19 +67,19 @@ export const handlers = [
     }
   ),
   rest.post(
-    "https://private-api-adzv.onrender.com/characters",
+    'https://private-api-adzv.onrender.com/characters',
     (req, res, ctx) => {
       return res(ctx.status(201));
     }
   ),
   rest.patch(
-    "https://private-api-adzv.onrender.com/characters/:id",
+    'https://private-api-adzv.onrender.com/characters/:id',
     (_req, res, ctx) => {
       return res(ctx.status(204));
     }
   ),
   rest.delete(
-    "https://private-api-adzv.onrender.com/characters/:id",
+    'https://private-api-adzv.onrender.com/characters/:id',
     (_req, res, ctx) => {
       return res(ctx.status(204));
     }
